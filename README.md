09586b4b2ee58a4e59f0b0b826a20d37ca3ef383
ZeroIntensity <49501366+ZeroIntensity@users.noreply.github.com>
2021-02-04 10:14:40 -0500

    Initial Commit.
    no way

---
+++ README.md
@@ -0,0 +1,6 @@
+# Last Updated: 2/4/21
+## Credits
+Made by me, ZeroIntensity using Bootstrap 4.
+
+## New Features
+If theres any new stuff I add i'll put it here, otherwise not much other to say than go visit [zintensity.net](https://zintensity.net)
---
+++ main.py
@@ -0,0 +1,62 @@
+import flask
+from threading import Thread
+from flask import Flask, redirect, url_for, render_template
+
+
+
+
+
+
+
+
+import flask
+from gevent.pywsgi import WSGIServer
+from threading import Thread
+from flask import request, jsonify, render_template
+
+app = flask.Flask(__name__, template_folder='pages')
+
+@app.route('/', methods=['GET'])
+def home():
+  return render_template('home.html')
+
+@app.route('/about', methods=['GET'])
+def about():
+  return render_template('about.html')
+
+@app.route('/contact', methods=['GET'])
+def contact():
+  return render_template('contact.html')
+
+@app.route('/projects/robocollab', methods=['GET'])
+def robocollab():
+  return render_template('robocollab.html')
+
+@app.route('/projects/zlyce', methods=['GET'])
+def zlyce():
+  return render_template('zlyce.html')
+
+@app.route('/projects/z.net', methods=['GET'])
+def znet():
+  return render_template('znet.html')
+
+@app.route('/projects/r.lol', methods=['GET'])
+def rlol():
+  return render_template('rlol.html')
+
+@app.route('/projects/planned', methods=['GET'])
+def planned():
+  return render_template('planned.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+  return render_template('404.html'), 404
+
+def run():
+  http_server = WSGIServer(('0.0.0.0', 8080), app)
+  http_server.serve_forever()
+
+
+server = Thread(target=run)
+server.start()
+
